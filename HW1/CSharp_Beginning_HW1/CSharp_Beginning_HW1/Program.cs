f5f9fb7c43dfd5d7a2fb2c760505f0f4d8589477
Turchin Michael <turchinm93@mail.ru>
2022-01-20 19:36:37 +0300

    Первое дз

---
+++ HW1/CSharp_Beginning_HW1/CSharp_Beginning_HW1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CSharp_Beginning_HW1
+{
+    class Program
+    {
+
+        /// <summary>
+        /// Программа первого занятия, написать программу, выводящую в консоль текст: «Привет, %имя пользователя%, сегодня %дата%»
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Здравствуйте! Введите имя пользователя ");
+
+           // String tempString = Console.ReadLine(); // можно обойтись без объявления переменной, дальше ведь мы её не используем
+
+            Console.WriteLine($"Привет, {Console.ReadLine()} сегодня - {DateTime.Now}"); // $ активирует возможность в {} скобках использовать переменные, либо запускать функции
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HW1/Запуск из системного терминала.bat
@@ -0,0 +1,1 @@
+start "" "CSharp_Beginning_HW1\CSharp_Beginning_HW1\bin\Debug\netcoreapp3.0\CSharp_Beginning_HW1.exe"
